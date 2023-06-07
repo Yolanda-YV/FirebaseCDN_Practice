fda52df0f0e5223afaf454e7caae816a283757f3
Yolanda-YV <yolanda.n.vega@gmail.com>
2023-06-07 13:38:31 -0400

    Create Login/SignUp
    Created 2 new js files then updated index with firebase CDN libraries and tags to make a simple login/signup page

---
+++ config.js
@@ -0,0 +1,18 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBkGaR1gMpmOAku_5jM75di3dhdbrpf9Dw",
+    authDomain: "practiceproject-32994.firebaseapp.com",
+    projectId: "practiceproject-32994",
+    storageBucket: "practiceproject-32994.appspot.com",
+    messagingSenderId: "411294226548",
+    appId: "1:411294226548:web:996b13e18d107c0cc6d921",
+    measurementId: "G-EXGPFEYF54"
+  };
+
+// Initialize firebase
+firebaseConfig.initializeApp(firebaseConfig);
+
+// make auth firebase references
+const auth = firebase.auth();
+const db = firebase.firestore();
+const analytics = firebase.analytics();
